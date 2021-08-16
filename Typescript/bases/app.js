0971844f7133adb2f37349c7fbbfa185bf5cf0b5
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-16 00:33:40 -0500

    13. Inferir tipos y modo estricto

--- Typescript/bases/app.js
+++ Typescript/bases/app.js
@@ -1,9 +1,6 @@
 "use strict";
-var msg = "Hola mundo";
-var hero = {
-    name: "Ironman",
-    age: 45
-};
-hero.age = 50;
-console.log(hero.age + 1);
-// 9. Modo observador - Watch mode
+(function () {
+    var a = 10;
+    console.log(a);
+})();
+// 13. Inferir tipos y modo estricto
--- Typescript/bases/app.ts
+++ Typescript/bases/app.ts
@@ -1,14 +1,9 @@
+(() => {
+    
+    const a:number = 10;
+    console.log(a);
+    
+})()
 
 
-const msg:string = "Hola mundo";
-
-const hero = {
-    name: "Ironman",
-    age: 45
-}
-
-hero.age = 50;
-console.log(hero.age + 1);
-
-
-// 9. Modo observador - Watch mode
+// 13. Inferir tipos y modo estricto
