ac1f2ba8ba57f87d195d1364db63877fb47f805c
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-16 18:38:55 -0500

    16. Strings - Cadenas de caracteres

---
+++ Typescript/bases/tipos/strings.js
@@ -0,0 +1,11 @@
+"use strict";
+(function () {
+    var _a;
+    var Batman = 'Batman';
+    var LinternaVerde = "Linterna Verde";
+    var VolcanNegro = "H\u00E9roe: Volc\u00E1n Negro";
+    console.log("I'm " + Batman);
+    console.log(Batman.toLocaleUpperCase());
+    console.log(((_a = Batman[10]) === null || _a === void 0 ? void 0 : _a.toLocaleUpperCase()) || "No está presente");
+})();
+// 16. Strings - Cadenas de caracteres
---
+++ Typescript/bases/tipos/strings.ts
@@ -0,0 +1,16 @@
+(() => {
+
+
+    const Batman : string = 'Batman'
+    const LinternaVerde : string = "Linterna Verde";
+    const VolcanNegro : string = `Héroe: Volcán Negro`;
+
+    console.log(`I'm ${ Batman }`);
+    console.log(Batman.toLocaleUpperCase());
+
+    console.log(Batman[10]?.toLocaleUpperCase() || "No está presente");
+    
+    
+})()
+
+// 16. Strings - Cadenas de caracteres
