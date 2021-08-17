b6e513aeb56daa835180156893eab70a5b25b8c5
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-16 22:05:13 -0500

    25. Tarea y Resolución del Ejercicio #1

--- Typescript/bases/app.js
+++ Typescript/bases/app.js
@@ -1,6 +1,37 @@
 "use strict";
 (function () {
-    var a = 10;
-    console.log(a);
+    // Tipos
+    var batman = 'Bruce';
+    var superman = 'Clark';
+    var existe = false;
+    // Tuplas
+    var parejaHeroes = [batman, superman];
+    var villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    var aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    // ? enum { acuaman = 0 }
+    var Fuerza;
+    (function (Fuerza) {
+        Fuerza[Fuerza["Acuaman"] = 0] = "Acuaman";
+        Fuerza[Fuerza["Batman"] = 1] = "Batman";
+        Fuerza[Fuerza["Flash"] = 5] = "Flash";
+        Fuerza[Fuerza["Superman"] = 100] = "Superman";
+    })(Fuerza || (Fuerza = {}));
+    var fuerzaFlash = Fuerza.Flash;
+    var fuerzaSuperman = Fuerza.Superman;
+    var fuerzaBatman = Fuerza.Batman;
+    var fuerzaAcuaman = Fuerza.Acuaman;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    var poder = '100';
+    var largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
-// 14. Booleans - Booleanos
+// 25. Tarea y Resolución del Ejercicio #1
--- Typescript/bases/app.ts
+++ Typescript/bases/app.ts
@@ -1,9 +1,49 @@
 (() => {
-    
-    const a:number = 10;
-    console.log(a);
-    
-})()
 
+    // Tipos
+    const batman:string = 'Bruce';
+    const superman:string = 'Clark';
+  
+    const existe:boolean = false;
+  
+    // Tuplas
+    const parejaHeroes:[string,string] = [batman,superman];
+    const villano:[string,number,boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados:string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+    // ? enum { acuaman = 0 }
+    enum Fuerza{
+      Acuaman = 0,
+      Batman = 1,
+      Flash = 5,
+      Superman = 100,
+    }
 
-// 14. Booleans - Booleanos
+    const fuerzaFlash:Fuerza = Fuerza.Flash;
+    const fuerzaSuperman:Fuerza = Fuerza.Superman;
+    const fuerzaBatman:Fuerza = Fuerza.Batman;
+    const fuerzaAcuaman:Fuerza = Fuerza.Acuaman;
+  
+    // Retorno de funciones
+    function activar_batiseñal():string{
+      return 'activada';
+    }
+  
+    function pedir_ayuda():void{
+      console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    console.log( largoDelPoder );
+  
+  
+  })()
+
+// 25. Tarea y Resolución del Ejercicio #1
+  
+  
