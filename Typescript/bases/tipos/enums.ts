06dd25f242eadd8d099767a797ae0c3805532ac4
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-16 20:59:56 -0500

    20. Emun - Enumeraciones

---
+++ Typescript/bases/tipos/enums.ts
@@ -0,0 +1,18 @@
+(()=>{
+
+
+    enum AudioLevel{
+        min = 1,
+        medium,
+        max = 10
+    }
+
+    let currentAudio:AudioLevel = AudioLevel.max;
+
+    console.log(currentAudio);
+    console.log(AudioLevel);
+    
+
+})()
+
+// 20. Emun - Enumeraciones
