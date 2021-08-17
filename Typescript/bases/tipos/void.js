a82bfb25fbf43ade8f1054dc6689885ae159c3f6
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-16 21:21:03 -0500

    21. Void - Vacío

---
+++ Typescript/bases/tipos/void.js
@@ -0,0 +1,12 @@
+"use strict";
+(function () {
+    function callBatman() {
+        return;
+    }
+    var callSuperman = function () {
+        return;
+    };
+    var a = callBatman();
+    console.log(a);
+})();
+// 21. Void - Vacío
