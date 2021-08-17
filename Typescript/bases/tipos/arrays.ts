1d9b9676bfcade06c6e5180c3827dac080544a37
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-16 20:25:05 -0500

    18. Arrays - Arreglos

---
+++ Typescript/bases/tipos/arrays.ts
@@ -0,0 +1,12 @@
+(()=>{
+
+
+    const numbers : number[] = [1,2,3,4,5,6,7,8,9,10];
+    
+    const villians : string[] = ["Omega Rojo","Dormammu","Duende Verde"];
+
+    villians.forEach( v => console.log( v.toLocaleUpperCase()));
+    
+})()
+
+// 18. Arrays - Arreglos
