e51b6b0782fcb0801ea96f340f6fc5227c72c1a1
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-17 16:24:29 -0500

    27. Funciones básicas

---
+++ Typescript/bases/funciones/functions.js
@@ -0,0 +1,13 @@
+"use strict";
+(function () {
+    var hero = "Flash";
+    function returnName() {
+        return hero;
+    }
+    var activateBatisignal = function () {
+        return "Batiseñal activada!";
+    };
+    console.log(typeof activateBatisignal);
+    var heroName = returnName();
+})();
+// 27. Funciones básicas
---
+++ Typescript/bases/funciones/functions.ts
@@ -0,0 +1,21 @@
+(()=>{
+
+
+    const hero:string = "Flash";
+
+    function returnName():string{
+        return hero;
+    }
+    
+    const activateBatisignal = ():string => {
+        return "Batiseñal activada!";
+    }
+
+    console.log(typeof activateBatisignal);
+
+    const heroName = returnName();
+    
+
+})()
+
+// 27. Funciones básicas
