bb1cbda6341224f4965a23db5841bf14c5b5f898
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-16 21:46:59 -0500

    23. Null y Undefined

---
+++ Typescript/bases/tipos/null-undefined.js
@@ -0,0 +1,7 @@
+"use strict";
+(function () {
+    // strictNullChecks
+    var isActive = undefined;
+    console.log(isActive);
+})();
+// 23. Null y Undefined
---
+++ Typescript/bases/tipos/null-undefined.ts
@@ -0,0 +1,10 @@
+(()=>{
+
+    // strictNullChecks
+    let isActive:(boolean|undefined) = undefined;
+
+    console.log(isActive);
+
+})()
+
+// 23. Null y Undefined
