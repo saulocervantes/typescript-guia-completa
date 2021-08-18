d0242aeae1554b329c74c1bed9ae04b4eb91fabc
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-18 00:29:23 -0500

    35. Objetos básicos

---
+++ Typescript/bases/objetos/objects.js
@@ -0,0 +1,17 @@
+"use strict";
+(function () {
+    var flash = {
+        name: "Barry Allen",
+        age: 24,
+        powers: ["Súper velocidad", "Viajar en el tiempo"]
+    };
+    flash = {
+        name: "Clark Kent",
+        age: 60,
+        powers: ["Súper fuerza"],
+        getNombre: function () {
+            return this.name;
+        }
+    };
+})();
+// 35. Objetos básicos
---
+++ Typescript/bases/objetos/objects.ts
@@ -0,0 +1,22 @@
+(()=>{
+
+
+    let flash = {
+        name: "Barry Allen",
+        age: 24,
+        powers: ["Súper velocidad", "Viajar en el tiempo"]
+    }
+
+    flash = {
+        name: "Clark Kent",
+        age: 60,
+        powers: ["Súper fuerza"]
+        getNombre(){
+            return this.name;
+        }
+    }
+
+
+})()
+
+// 35. Objetos básicos
