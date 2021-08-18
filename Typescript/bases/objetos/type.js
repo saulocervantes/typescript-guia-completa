56d9a598ae2de5550a0f68be1742b12747d04a7c
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-18 01:06:28 -0500

    39. Tipos personalizados

---
+++ Typescript/bases/objetos/type.js
@@ -0,0 +1,17 @@
+"use strict";
+(function () {
+    var flash = {
+        name: "Barry Allen",
+        age: 24,
+        powers: [1, 2]
+    };
+    var superman = {
+        name: "Clark Kent",
+        age: 60,
+        powers: [1],
+        getName: function () {
+            return this.name;
+        }
+    };
+})();
+// 39. Tipos personalizados
